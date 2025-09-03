a0b3f38426241134b6e705f93d3df3604ce8f16a
doctorpangloss <@hiddenswitch.com>
2025-09-03 14:52:08 -0700

    wip

--- tests/issues/__test_30_comfy_workspace_conflicts.py
+++ tests/issues/__test_30_comfy_workspace_conflicts.py
@@ -7,7 +7,7 @@ source .venv/bin/activate
 uv pip install --torch-backend=auto requirements.txt
 
 # install livepeer with up to date comfyui lts
-uv pip install --torch-backend=auto git+https://github.com/livepeer/comfystream.git --overrides=<(echo "comfyui@git+https://github.com/hiddenswitch/ComfyUI.git")
+uv pip install --torch-backend=auto git+https://github.com/livepeer/comfystream.git --overrides=<(echo "comfyui@git+https://github.com/hiddenswitch/ComfyUI.git@fixes/issue-30")
 
 # install the nodes
 curl -L -o comfystream.zip https://github.com/livepeer/comfystream/archive/refs/heads/main.zip
